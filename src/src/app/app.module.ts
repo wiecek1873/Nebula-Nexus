75c8dd1b4e9b50c1555faf63f0df3cd85deab11c
Michał Więcek <wiecek1873@gmail.com>
2023-07-27 22:48:52 +0200

    Works with standalone module

--- src/src/app/app.module.ts
+++ src/src/app/app.module.ts
@@ -5,14 +5,14 @@ import { RouterModule } from '@angular/router';
 import { GettingStartedComponent } from './getting-started/getting-started.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
+// import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    MaterialModule,
+    // MaterialModule,
     BrowserModule,
     RouterModule.forRoot([
       { path: 'getting-started', component: GettingStartedComponent },
--- src/src/app/getting-started/getting-started.component.ts
+++ src/src/app/getting-started/getting-started.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+
 
 @Component({
   selector: 'app-getting-started',
   templateUrl: './getting-started.component.html',
-  styleUrls: ['./getting-started.component.scss']
+  styleUrls: ['./getting-started.component.scss'],
+  standalone: true,
+  imports: [MatButtonModule]
 })
 export class GettingStartedComponent {
 
