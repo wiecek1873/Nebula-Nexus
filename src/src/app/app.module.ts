4ef0e5ce87f83ec3e92de35dca70c5ad1c5aecdd
Michał Więcek <wiecek1873@gmail.com>
2023-07-31 21:52:37 +0200

    basic routing to sign up page

--- src/src/app/app.module.ts
+++ src/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: 'sign-up', component: SignUpPageComponent },
+      { path: '**', redirectTo: 'sign-up' }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
